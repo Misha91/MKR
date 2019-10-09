c1fe8f426cfb40f2579b852776d921ce899d1258
Misha Ivanov <mikhail.ivanov@rocketmail.com>
2019-10-09 13:40:38 +0200

    correct path for input2

--- src/motion/scripts/robot_input.py
+++ src/motion/scripts/robot_input.py
@@ -13,7 +13,7 @@ if (path.split('/')[-1] != 'mkr_tm_ws'):
     path += '/mkr_tm_ws'
 
 print(path)
-with open('src/motion/data/robot_start_positions.csv', mode='r') as csv_file:
+with open(path + '/src/motion/data/robot_start_positions.csv', mode='r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter = ',')
     for row in csv_reader:
         x = float(row[0])
@@ -23,7 +23,7 @@ with open('src/motion/data/robot_start_positions.csv', mode='r') as csv_file:
         new_robot = Robot([x,y,theta],goal)
         robots.append(new_robot)
 
-with open('src/motion/data/robot_data.csv', mode='w') as robot_file:
+with open(path + '/src/motion/data/robot_data.csv', mode='w') as robot_file:
     robot_writer = csv.writer(robot_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for robot in robots:
        robot_writer.writerow([robot.name, robot.start_pose[0], robot.start_pose[1], robot.start_pose[2], robot.goal_pose[0], robot.goal_pose[1]])

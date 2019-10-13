58e40ef78240dbd11c05ae172c95f32caf1733c0
Timur <uzakotim@fel.cvut.cz>
2019-10-13 20:10:34 +0200

    A star with full square neighbourhood consideration

--- src/motion/scripts/planner.py
+++ src/motion/scripts/planner.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 from utils import insort, manhattan_dist
 
 def plan(map, start, goal):
@@ -9,10 +8,11 @@ def plan(map, start, goal):
         visited_poses = []
         priority_queue = []
         priority_poses = []
+        priority_lens = []
         x_bound = map.shape[0]
         y_bound = map.shape[1]
         occupancy_threshold = 0
-        
+
         if goal[0]<0 or goal[0]>x_bound:
             raise Exception("Goal is out of x bound")
         if goal[1]<0 or goal[1]>y_bound:
@@ -23,7 +23,7 @@ def plan(map, start, goal):
             raise Exception("Goal is in unexplored region")
 
         """configuration of node search"""
-        list_of_directions = [[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1]]
+        list_of_directions = [[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1],[0,0]]
 
         """initialization"""
         ### building starting node
@@ -35,7 +35,7 @@ def plan(map, start, goal):
         visited_poses.append(new_parent[1])
 
         """node iterations"""
-        while(visited_nodes[-1][1]!=goal):
+        while(new_parent[1]!=goal):
 
             parent_pose = new_parent[1]
             parent_path = new_parent[2]
@@ -44,9 +44,10 @@ def plan(map, start, goal):
             for x in list_of_directions:
 
                 """node creation"""
-                new_node = [manhattan_dist([parent_pose[0]+x[0],parent_pose[1]+x[1]], goal), #D
+                history = list(parent_path) + [[parent_pose[0]+x[0],parent_pose[1]+x[1]]]
+                new_node = [manhattan_dist([parent_pose[0]+x[0],parent_pose[1]+x[1]], goal) + len(history), #D
                                 [parent_pose[0]+x[0],parent_pose[1]+x[1]], # new pose
-                                list(parent_path) + [[parent_pose[0]+x[0],parent_pose[1]+x[1]]] #path
+                                history #path
                                 ]
                 """filtering"""
                 ### within map bounds ###
@@ -57,24 +58,30 @@ def plan(map, start, goal):
                 ### not occupied ###
                 if map[new_node[1][0]][new_node[1][1]]>occupancy_threshold:
                     continue
+
                 ### not considered before ###
-                if new_node[1] in visited_poses:
-                    continue
-                if new_node[1] in priority_poses:
+                #if new_node[1] in visited_poses:
+                #    continue
+
+                if new_node[1] in priority_poses and len(new_node[2]) in priority_lens:
                     continue
 
                 """adding to queue"""
                 priority_queue = insort(priority_queue, new_node)
                 priority_poses.append(new_node[1])
+                priority_lens.append(len(new_node[2]))
 
             """selection of new parent node"""
+            print(new_parent[1])
             new_parent = priority_queue[0]
 
-            visited_nodes.append(new_parent)
-            visited_poses.append(new_parent[1])
+            # visited_nodes.append(new_parent)
+            # visited_poses.append(new_parent[1])
 
             priority_queue.pop(0)
             priority_poses.pop(priority_poses.index(new_parent[1]))
+            priority_lens.pop(len(new_parent[2]))
+
 
         """return path"""
-        return (visited_nodes[-1][2])
+        return (new_parent[2])

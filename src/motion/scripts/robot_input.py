2e66116ed279cab0c7664c59a2defe64c616414f
Misha Ivanov <mikhail.ivanov@rocketmail.com>
2019-10-16 17:36:09 +0200

    Working solution for moving by waypoint

--- src/motion/scripts/robot_input.py
+++ src/motion/scripts/robot_input.py
@@ -20,28 +20,38 @@ path = os.getcwd()
 robots = []
 if (path.split('/')[-1] != 'mkr_tm_ws'):
     path += '/mkr_tm_ws'
-print(path)
+
+rospy.init_node("path_planner")
+
+"""Getting map"""
+pool_kernel = 20
+init_map = rospy.wait_for_message("/map", OccupancyGrid)
+print(init_map.info.height, init_map.info.width, init_map.info.resolution)
+map_res = init_map.info.resolution
+map = np.reshape(init_map.data, (init_map.info.height, init_map.info.width)).T
+map = skimage.measure.block_reduce(map, (pool_kernel, pool_kernel), np.max)
+print(map.shape)
+# print(map)
 
 """Reading initial positions and goals of robots"""
 
 with open(path + '/src/motion/data/robot_start_positions.csv', mode='r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter = ',')
     for i,row in enumerate(csv_reader):
-        if i != 0:
-            x = int(row[0])
-            y = int(row[1])
-            theta = float(row[2])
-            start = [x,y,theta]
-            goal = [int(row[3]),int(row[4])]
-            new_robot = Robot(start,goal)
-            robots.append(new_robot)
+        if (i == 0): continue
+        goal = [int(row[3]),int(row[4])]
+        new_robot = Robot(i -1, map_res, pool_kernel, goal)
+        #new_robot.pose_coord = [int(new_robot.pose.x/(map_res*pool_kernel)), int(new_robot.pose.y/(map_res*pool_kernel))]
+        robots.append(new_robot)
 
 """Writing data file with names"""
 
 with open(path + '/src/motion/data/robot_data.csv', mode='w') as robot_file:
     robot_writer = csv.writer(robot_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for robot in robots:
-       robot_writer.writerow([robot.name, robot.start_pose[0], robot.start_pose[1], robot.start_pose[2], robot.goal_pose[0], robot.goal_pose[1]])
+
+        print(int(robot.pose.x/(map_res*pool_kernel)), int(robot.pose.y/(map_res*pool_kernel)))
+        robot_writer.writerow([robot.name, robot.start_pose[0], robot.start_pose[1], robot.goal_pose[0], robot.goal_pose[1]])
 
 """Not necessary, use robots list directly"""
 #robot_list = []
@@ -54,21 +64,17 @@ with open(path + '/src/motion/data/robot_data.csv', mode='w') as robot_file:
 for robot in robots:
     print(robot.name,robot.start_pose, robot.goal_pose)
 
-"""Getting map"""
-rospy.init_node("path_planner")
-init_map = rospy.wait_for_message("/map", OccupancyGrid)
-map = np.reshape(init_map.data, (init_map.info.height, init_map.info.width)).T
-map = skimage.measure.block_reduce(map, (20,20), np.max)
-print(map.shape)
-# print(map)
+
 """Planning"""
 
 morph_size = 3
 
 morph_map = morphology.grey_dilation(map, size=(morph_size,morph_size))
-robot_path = plan(morph_map, robots[0].start_pose[:2],robots[0].goal_pose)
+
+robots[1].waypoint = plan(morph_map, robots[1].start_pose, robots[1].goal_pose)
 print("Robot path")
-print(robot_path)
+print(robots[1].waypoint)
+robot_path = robots[1].waypoint
 
 #map = multi_plan(map, robots)
 """PRINTING OF MAP"""
@@ -81,3 +87,5 @@ for j in (reversed(range(map.shape[1]))):
         else: str += " "
     print(str)
 print("UPDATED")
+
+robots[1].goToFinalGoal()

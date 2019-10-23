2a88a2e8dc3108fd34a1d5ed59574f40d310a288
Timur <uzakotim@fel.cvut.cz>
2019-10-23 15:04:56 +0200

    Cleaning and finalization

--- src/motion/scripts/robot_class.py
+++ src/motion/scripts/robot_class.py
@@ -30,24 +30,13 @@ class Robot(object):
         else:
             self.start_pose = self.coordToGrid(self.pose.x, self.pose.y)
             self.goal_pose = goal
-        self.priority_queue = []
-        self.priority_poses = []
-        self.priority_lens = []
-        self.start_node = []
-        self.new_parent = []
-        self.is_finished = False
+
         self.waypoint = []
         rospy.set_param('~' + self.nameId, self.nameId)
         self.map = []
-        self.wp_iter = 1
-        #tmp_init = self.gridToCoord(self.goal_pose[0],self.goal_pose[1])
-        #self.goalFinal = Point(tmp_init[0], tmp_init[1], 0)
-        #self.odomSub = rospy.Subscriber(self.nameId + "/odom/", Odometry, self.odomUpdate)
-        #self.velPub = rospy.Publisher(self.nameId + "/cmd_vel", Twist, queue_size = 1)
-        #self.r = rospy.Rate(100)
 
     def reset(self):
-        prev_name = self.name
+        prev_name = self.name    #thread.start_new_thread(start_movement, (robot.nameId))
         new_name = prev_name
         while prev_name == new_name:
             new_name = "".join(choice(self.letters)
@@ -80,97 +69,3 @@ class Robot(object):
             rospy.logerr('Could not add path, exiting')
             return
         rospy.loginfo('Path added')
-        """
-        if not start_movement(server_ns):
-            rospy.logerr('Could not start motion, exiting')
-            return
-        rospy.loginfo('Movement started')
-
-        time.sleep(20)
-
-        if not stop_movement(server_ns):
-            rospy.logerr('Could not stop motion, exiting')
-            return
-        rospy.loginfo('Movement stopped')
-
-        if not add_path(server_ns, []):
-            rospy.logerr('Could not clear path, exiting')
-            return
-        rospy.loginfo('Path cleared')
-        """
-
-    """
-
-    def odomUpdate(self, msg):
-        self.pose.x = msg.pose.pose.position.x
-        self.pose.y = msg.pose.pose.position.y
-        rot_q = msg.pose.pose.orientation
-        (roll, pitch, theta) = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
-        self.pose.z = theta
-        #print(self.id, self.pose)
-
-    def getDistance(self, A, B):
-        return math.sqrt((A.x-B.x)**2 + (A.y-B.y)**2)
-
-    def goToFinalGoal(self):
-        print("HERE WE GO!")
-        tmp = 0
-        while(self.getDistance(self.pose, self.goalFinal) >= 0.1):
-            #print("DISTANCE: " + str(self.getDistance(self.pose, self.goalFinal)))
-            self.keepGoing()
-            #tmp += 1
-            #if (tmp >= 3000): break
-
-        self.speed.linear.x = 0.0
-        self.speed.angular.z = 0.0
-        self.velPub.publish(self.speed)
-
-    def keepGoing(self):
-
-        goal_coord = self.gridToCoord(self.waypoint[self.wp_iter][0], self.waypoint[self.wp_iter][1])
-        goal_coord = Point(goal_coord[0], goal_coord[1], 0)
-        cur_dist = self.getDistance(self.pose, goal_coord)
-        print("keep going!")
-        if cur_dist < 0.1:
-            if (self.wp_iter < (len(self.waypoint)-1)):
-                self.wp_iter += 1
-                print("Going to point #" + str(self.wp_iter) + "/" + str(len(self.waypoint)))
-
-        #print(self.wp_iter, self.pose, goal_coord)
-        print("here!")
-        inc_x = goal_coord.x - self.pose.x
-        inc_y = goal_coord.y - self.pose.y
-
-        angle_to_goal = math.atan2(inc_y, inc_x)
-        diff = angle_to_goal - self.pose.z
-        #print(goal_coord, self.pose)
-        #print(angle_to_goal, self.pose.z, diff)
-        if abs(diff) > 0.1:
-            #print(diff)
-            if (self.wp_iter >= 2):
-                self.speed.linear.x = 0.0
-            self.speed.angular.z = 1.5
-            if (diff <= 3):
-                self.speed.angular.z = self.speed.angular.z * np.sign(diff)
-        else:
-            self.speed.linear.x = 1.0
-            self.speed.angular.z = 0.0
-
-        self.velPub.publish(self.speed)
-        print("here2")
-        self.r.sleep()
-    """
-
-########################################
-### You can check if it works ##########
-#robot1 = Robot([0,2],[0,2.])
-##robot1 = Robot(0.3,"bullshit")
-##robot2 = Robot([0,"bullshit"],[5,10])
-#robot2 = Robot([0,1],[10,0])
-#print("Robot's names", robot1.name , robot2.name)
-#robot1.reset()
-#robot2.reset()
-#print("New robots' names", robot1.name, robot2.name)
-#print("Robot 1 goes from", robot1.start_pose, "to ",robot1.goal_pose)
-#########################################
-#########################################
--- src/motion/scripts/robot_input.py
+++ src/motion/scripts/robot_input.py
@@ -9,7 +9,7 @@ import subprocess
 from planner import plan
 from planner_multi import multi_plan
 from coord_helper import *
-import thread
+
 
 from nav_msgs.msg import OccupancyGrid
 import numpy as np
@@ -35,7 +35,6 @@ map_res = init_map.info.resolution
 map = np.reshape(init_map.data, (init_map.info.height, init_map.info.width)).T
 map = skimage.measure.block_reduce(map, (pool_kernel, pool_kernel), np.max)
 print(map.shape)
-# print(map)
 
 """Reading initial positions and goals of robots"""
 
@@ -45,7 +44,6 @@ with open(path + '/robot_start_positions.csv', mode='r') as csv_file:
         if (i == 0): continue
         goal = [int(row[3]),int(row[4])]
         new_robot = Robot(i -1, map_res, pool_kernel, goal)
-        #new_robot.pose_coord = [int(new_robot.pose.x/(map_res*pool_kernel)), int(new_robot.pose.y/(map_res*pool_kernel))]
         robots.append(new_robot)
 
 """Writing data file with names"""
@@ -57,93 +55,10 @@ with open(path + '/robot_data.csv', mode='w') as robot_file:
         print(int(robot.pose.x/(map_res*pool_kernel)), int(robot.pose.y/(map_res*pool_kernel)))
         robot_writer.writerow([robot.name, robot.start_pose[0], robot.start_pose[1], robot.goal_pose[0], robot.goal_pose[1]])
 
-"""Not necessary, use robots list directly"""
-#robot_list = []
-#for robot in robots:
-#    tmp = Robot(robot.start_pose, robot.goal_pose)
-#    robot_list.append(tmp)
-#    print("Robot:", robot.name,"Position:", robot.start_pose, "Goal:", robot.goal_pose)
-
 """Print robot data"""
 for robot in robots:
     print(robot.name,robot.start_pose, robot.goal_pose)
 
-
-"""Planning"""
-
-
-"""
-#Test waypoint
-morph_size = 3
-
-morph_map = morphology.grey_dilation(map, size=(morph_size,morph_size))
-
-robots[1].waypoint = plan(morph_map, robots[1].start_pose, robots[1].goal_pose)
-print("Robot path")
-print(robots[1].waypoint)
-robot_path = robots[1].waypoint
-
-#map = multi_plan(map, robots)
-#PRINTING OF MAP
-
-for j in (reversed(range(map.shape[1]))):
-    str = ""
-    for i in range(map.shape[0]):
-        if (map[i][j] >0) or ([i,j] in robot_path):
-            str += "#"
-        else: str += " "
-    print(str)
-print("UPDATED")
-
-robots[1].goToFinalGoal()
-#end of testing waypoint
-"""
-
-# morph_size = 3
-
-# morph_map = morphology.grey_dilation(map, size=(morph_size,morph_size))
-# robot_path = plan(morph_map, robots[0].start_pose[:2],robots[0].goal_pose)
-# print("Robot path")
-# print(robot_path)
-#
-# list_of_pathes = []
-# new_robots = multi_plan(map, robots)
-# for robot in new_robots:
-#      print(robot.waypoint)
-#      list_of_pathes.append(robot.waypoint)
-#
-# list_of_pathes.sort(lambda x,y: len(x) < len(y))
-#
-# plt.ion()
-# plt.show()
-# p = []
-# cmap = colors.ListedColormap(['white','black'])
-# for i in range(len(list_of_pathes[0])):
-#     new_map = np.copy(map)
-#     # if i < len(list_of_pathes[-1]):
-#     #     for robot in new_robots:
-#     #         p[robot.waypoint[i][0]][robot.waypoint[i][1]] = 100
-#     # plt.plot(list_of_pathes[0][i][0],list_of_pathes[0][i][1],'bo')
-#     # plt.plot(list_of_pathes[1][i][0],list_of_pathes[1][i][1],'r+')
-#
-#
-#     if i < len(list_of_pathes[-1]):
-#         new_map[list_of_pathes[1][i][0]][list_of_pathes[1][i][1]]= 100
-#     new_map[list_of_pathes[0][i][0],list_of_pathes[0][i][1]] = 100
-#     plt.imshow(new_map,cmap = cmap)
-#
-#     plt.pause(0.5)
-
-# """PRINTING OF MAP"""
-#
-# for j in (reversed(range(map.shape[1]))):
-#     str = ""
-#     for i in range(map.shape[0]):
-#         if (map[i][j] >0) or ([i,j] in robot_path):
-#             str += "#"
-#         else: str += " "
-#     print(str)
-# print("UPDATED")
 """ MULTI-PLANNING """
 # input: map, robot list
 # output robot list with modified self.waypoint
@@ -153,9 +68,4 @@ for robot in planned_robots:
     robot.startMoving()
 
 for robot in planned_robots:
-    #thread.start_new_thread(start_movement, (robot.nameId))
     start_movement(robot.nameId)
-    #if not start_movement(robot.nameId):
-    #    rospy.logerr('Could not start motion, exiting')
-        #return
-    #rospy.loginfo('Movement started')

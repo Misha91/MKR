c4c56fedc3d639bd6ade403a0ab3747cdc7c1bed
Timur <uzakotim@fel.cvut.cz>
2019-11-10 15:28:11 +0100

    Kalman filter updated

--- src/student/kf_main.cpp
+++ src/student/kf_main.cpp
@@ -33,20 +33,20 @@ Point prevMeas (0,0);
 
 MatrixXf A(4,4);
 MatrixXf B(4,1);
-MatrixXf Sigma;  // Error covariance matrix. Sometimes called P.
 MatrixXf Q(4,4); // Covariance of the process noise.
 MatrixXf R(4,4);  // Covariance of the observation noise
 MatrixXf C(4,4);  // Observation matrix. Sometimes called H.
 MatrixXf S(4,4);
 MatrixXf P(4,4);
+MatrixXf P_est(4,4);
 MatrixXf K(4,4);
-VectorXf xu(4);  // State vector. Sometimes called X.
-VectorXf xn(4);
-VectorXf zu(4);  // State vector. Sometimes called X.
-VectorXf y_t(4);  // State vector. Sometimes called X.
+MatrixXf I(4,4);
+VectorXf cur_state(4);  // State vector. Sometimes called X.
+VectorXf x_predict(4);
+VectorXf cur_meas(4);  // State vector. Sometimes called X.
+VectorXf innovation(4);
 float dt = 0.1;
 float g = 9.81;
-
 
 
 void help(char** argv)
@@ -61,7 +61,6 @@ int main(int argc, char** argv)
 {
     int nSteps = 1000;
     char *dataFile;
-
     // Parse all console parameters
     for (int i = 0; i < argc; i++)
     {
@@ -86,77 +85,53 @@ int main(int argc, char** argv)
         }
     }
     // All parameters parsed
-    printf("A\n");
     A << 1, 0, dt, 0,
          0, 1, 0, -dt,
          0, 0, 1, 0,
          0, 0, 0, 1;
-
-    std::cout << A;
-
-    printf("\n********************\n");
-    printf("B\n");
     float tmp_helper = -0.5*dt*dt;
     B << 0, tmp_helper, 0, -dt;
-    std::cout << B;
-
-    printf("\n******************\n");
-    printf("C\n");
     C << MatrixXf::Identity(4, 4);
-    std::cout << C;
-    printf("\n******************\n");
-    printf("R\n");
-    R << MatrixXf::Identity(4, 4);
-    R(0,0) = 30*30;
-    R(1,1) = 30*30;
-    R(2,2) = 400*400;
-    R(3,3) = 400*400;
-    std::cout << R;
-    printf("\n******************\n");
-    printf("Q\n");
+// """ MEASUREMENT UNCERTAINTY """
     Q << MatrixXf::Identity(4, 4);
-    Q(0,0) = 30*30;
-    Q(1,1) = 30*30;
-    Q(2,2) = 400*400;
-    Q(3,3) = 400*400;
-    std::cout << Q;
-    printf("\n******************\n");
-    printf("P\n");
+    Q(0,0) = 25;
+    Q(1,1) = 25;
+    Q(2,2) = 4;
+    Q(3,3) = 4;
+// """ PREDICTION UNCERTAINTY """
+    R << MatrixXf::Identity(4, 4);
+    R(0,0) = 0.5;
+    R(1,1) = 0.5;
+    R(2,2) = 0.5;
+    R(3,3) = 0.5;
+
+    I << MatrixXf::Identity(4,4);
     P << MatrixXf::Zero(4, 4);
-
-    std::cout << P;
-    printf("\n******************\n");
+    P_est << MatrixXf::Zero(4, 4);
 
 
-    xu << 0, 0, 0, 0;
-    xn << 0, 0, 0, 0;
-    zu << 0, 0, 0, 0;
-    y_t << 0, 0, 0, 0;
+    cur_state << 0, 0, 0, 0;
+    cur_meas << 0, 0, 0, 0;
+    innovation << 0, 0, 0, 0;
 
     Gui gui;
     System system;
 
     //> comment line below in order to let the program
-    //> continue right away
+    //> continue right awayprintf("predicted");
     gui.startInteractor();
 
     Point measurement;
     Point truth;
     Point kfPosition;
-
     for (int i = 1; i < nSteps; i++)
     {
+
         system.makeStep();
         truth = system.getTruthPosition();
         measurement = system.getMeasurement();
-        //printf("%d %f %f\n", i, -(prevMeas.x-truth.x)/dt, -(prevMeas.y-truth.y)/dt);
-
         kfPosition = KalmanFilter(measurement);
         gui.setPoints(truth, measurement, kfPosition);
-        printf("********************");
-        printf("\ni = %d\t %.2f %.2f --- %.2f %.2f\nXn:\n", i, kfPosition.x, kfPosition.y, truth.x, truth.y);
-        std::cout << xn;
-        printf("\n");
         //> comment line below in order to let the program
         //> continue right away
         if (i % 20 == 0)
@@ -178,27 +153,20 @@ int main(int argc, char** argv)
 Point  KalmanFilter(const Point & measuredPosition)
 {
 
-  zu(2) = -(measuredPosition.x - zu(2))/dt;
-  zu(3) = -(measuredPosition.y - zu(3))/dt;
-  zu(0) = measuredPosition.x;
-  zu(1) = measuredPosition.y;
-
-  static int init_checker = 0;
-
-  xu =  A * xn + B*g;
-
-  P = A * P * A.transpose() + Q;
-  y_t = zu - C * xu;
-  S = C * P * C.transpose() + R;
-  K = P * C.transpose() * S.inverse();
-  printf("\n*****XU******\n");
-  std::cout << xu;
-  printf("\n*****K ******\n");
-  std::cout << (K);
-  xn = xu + K * y_t;
-  prevMeas = measuredPosition;
-
-  init_checker++;
-
-  return Point(xn(0), xn(1));
+  cur_meas(0) = measuredPosition.x;
+  cur_meas(1) = measuredPosition.y;
+  cur_meas(2) = 0.01*(measuredPosition.x - cur_state(2))/(abs(measuredPosition.x - cur_state(2))*dt);
+  cur_meas(3) = 0.01*(measuredPosition.y - cur_state(3))/(abs(measuredPosition.x - cur_state(2))*dt);
+// prediction
+  x_predict =  A * cur_state + B*g;
+  P_est = A * P * A.transpose() + R;
+// correction
+  S = C * P_est * C.transpose() + Q;
+  K = P_est * C.transpose() * S.inverse();
+  innovation = cur_meas - C * x_predict;
+  cur_state = x_predict + K * innovation;
+// update covariance
+  P = (I - K*C)*P_est;
+// prevMeas = measuredPosition;
+  return Point(cur_state(0), cur_state(1));
 }
